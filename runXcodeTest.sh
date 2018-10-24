4dccf699dc3861c1e2cd18c324b46735073305b3
Ryuichi Sai <ryuichi@ryuichisai.com>
2018-10-23 19:16:49 -0500

    Additional code cleaning up for Swift 4.2

--- runXcodeTest.sh
+++ runXcodeTest.sh
@@ -2,4 +2,4 @@
 
 make xcodegen
 WORKING_DIRECTORY=$(PWD) xcodebuild -project swift-transform.xcodeproj -scheme swift-transform-Package clean
-WORKING_DIRECTORY=$(PWD) xcodebuild -project swift-transform.xcodeproj -scheme swift-transform-Package -sdk macosx10.13 -destination arch=x86_64 -configuration Debug -enableCodeCoverage YES test
+WORKING_DIRECTORY=$(PWD) xcodebuild -project swift-transform.xcodeproj -scheme swift-transform-Package -sdk macosx10.14 -destination arch=x86_64 -configuration Debug -enableCodeCoverage YES test
